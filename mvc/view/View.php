47032158a12a5e61669c784ac1139ca1bfe7cf0b
Bo-Eamonn <bo.eamonn@gmail.com>
2020-11-24 22:48:30 +0100

    Bo | Started with making a view file of some kind

---
+++ mvc/view/View.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace view;
+include_once ('model/Model.php');
+include_once ('model/User.php');
+include_once ('model/Patient');
+include_once ('model/Med.php');
+
+class View{
+
+    private $model;
+    public function __construct($model){
+        $this->model = $model;
+    }
+    public function showUsers($result = null){
+        if($result ==1){
+            echo "TEST TEST... Working";
+        }
+        $users = $this->model->getUsers();
+        
+        include_once('templates/header.php');
+        include_once('templates/nav.php');
+        
+        
+        
+        EOT;
+        
+    }
+
+}
