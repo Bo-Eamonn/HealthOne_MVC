3f191bf949018a8387ad008235924155737002ec
Bo-Eamonn <bo.eamonn@gmail.com>
2020-12-01 14:39:00 +0100

    Bo | Removed head seaction

--- templates/header.php
+++ templates/header.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="nl">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HealthOne</title>
-    <link rel="stylesheet" href="/HealthOne/frontend/static/css/user.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-    <script type="text/javascript" src="/healthone/frontend/static/js/search.js"></script>
-</head>
 
 <body>
     <header>
@@ -24,7 +12,7 @@
             </div>
             <div>
                 <button id="logout" title="uitloggen"
-                    onclick="location.href='/HealthOne/backend/logout.php'">Uitloggen</button>
+                    onclick="location.href=' '">Uitloggen</button>
             </div>
         </div>
     </header>
