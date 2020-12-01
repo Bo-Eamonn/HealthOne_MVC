40cfb55c7fce60108406eec4a520fc899aa1ef88
Bo-Eamonn <bo.eamonn@gmail.com>
2020-12-01 13:08:09 +0100

    Bo | Started Making controller

---
+++ mvc/controller/Controller.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace mvc\controller;
+include_once "mvc/view/View.php";
+use mvc\view\View;
+include_once "mvc/model/Patient.php";
+use mvc\model\Patient;
+include_once "mvc/model/Med.php";
+use mvc\model\Med;
+include_once "mvc/model/Model.php";
+use mvc\model\Model;
+
+class Controller{
+    private $view;
+    private $model;
+    public function __construct(){
+        $this->model = new Model();
+        $this->view = new View($this->model);
+    }
+    public function showMedAction(){
+        $result = $this->model->getMed();
+        $this->view->showMed($result);
+    }
+    public function showUserAction(){
+        $result = $this->model->getUsers();
+        $this->view->showUsers($result);
+    }
+    public function showPatientAction(){
+        $result = $this->model->getPatients();
+        $this->view->showPatients($result);
+    }
+
+
+}
