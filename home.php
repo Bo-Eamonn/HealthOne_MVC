b6c45c310347dcdff1fd85cf6080af79065a90b4
Bo-Eamonn <bo.eamonn@gmail.com>
2020-12-01 14:40:27 +0100

    Bo | Changed content to different page and added login, needs to change to mvc structur

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+
+use mvc\controller\Controller;
+
+include_once "mvc/controller/Controller.php"; 
+$controller = new Controller();
+$controller->showMedAction();
+$controller->showUserAction();
+$controller->showPatientAction();
