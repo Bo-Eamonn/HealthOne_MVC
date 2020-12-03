72c5db4a803375947b6805b543e395644eebadec
Bo-Eamonn <bo.eamonn@gmail.com>
2020-12-03 02:19:41 +0100

    Bo | Deleted file

--- home.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use mvc\controller\Controller;
-
-include_once "mvc/controller/Controller.php"; 
-$controller = new Controller();
-$controller->showMedAction();
-$controller->showUserAction();
-$controller->showPatientAction();
