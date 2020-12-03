4eae380ac39a7f9ab4a70daecd457b0b0ef64e64
Bo-Eamonn <bo.eamonn@gmail.com>
2020-12-03 01:32:30 +0100

    Bo | Changed my mind everthing goes in index

--- index.php
+++ index.php
@@ -4,4 +4,13 @@ use mvc\controller\Controller;
 
 include_once "mvc/controller/Controller.php"; 
 $controller = new Controller();
-$controller->login();
+
+// if(isset($_SESSION['role']) && $_SESSION['role']==3)
+// {
+    $controller->showMedAction();
+    $controller->showUserAction();
+    $controller->showPatientAction();
+
+// } else {
+//     $controller->loginAction();
+// }
