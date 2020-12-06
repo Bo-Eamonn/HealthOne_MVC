f24bd737dcad3f0154dd065d184d2bc8cfcfc3bb
Bo-Eamonn <bo.eamonn@gmail.com>
2020-12-06 23:19:38 +0100

    Bo | Updated for all med-crud functions

--- home.php
+++ home.php
@@ -11,9 +11,15 @@ $controller = new Controller();
             $controller->logoutAction();
         }elseif (isset($_POST['med'])) {
             $controller->showMedAction();
-        }elseif (isset($_POST['addMed'])) {
+        }elseif (isset($_POST['showAddMed'])) {
             $controller->addMedAction();
-        } elseif (isset($_POST['user'])) {
+        }elseif (isset($_POST['toevoegenMed'])) {
+            $controller->saveMed();
+        } elseif(isset($_POST['deleteMed'])) {
+            $controller->deleteMed();
+        } elseif(isset($_POST['editMed'])) {
+            $controller->editMed();
+        }elseif (isset($_POST['user'])) {
             $controller->showUserAction();
         } elseif (isset($_POST['patient'])) {
             $controller->showPatientAction();
