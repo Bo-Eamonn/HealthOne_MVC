63107a29c236bfec4f30ff5f4e0c0781ed31e885
Bo-Eamonn <bo.eamonn@gmail.com>
2020-12-07 00:14:30 +0100

    Bo | Fixed styling

--- templates/med/addMed.php
+++ templates/med/addMed.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HealthOne | Toevoegen Medicijnen</title>
+    <link rel="stylesheet" href="/HealthOne_MVC/assets/css/user.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+</head>
+
+<?php
+require_once "templates/header.php";
+require_once "templates/nav.php";
+?>
 <div id="medAddModal" class="modal">
     <div class="medModal-container">
         <div>
@@ -44,7 +60,7 @@
                     </tr>
                     <tr>
                         <td></td>
-                        <td><input type='submit' name='toevoegenMed' value='opslaan'></td>
+                        <td><input onclick="" type='submit' name='toevoegenMed' value='opslaan'></td>
                         <td></td>
                     </tr>
                 </table>
@@ -52,3 +68,6 @@
         </div>
     </div>
 </div>
+<?php
+require_once "templates/footer.php";
+?>
